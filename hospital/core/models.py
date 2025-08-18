f22961b93df99207f0f696561cbbee7ff06e5694
Ishan <211b145@juetguna.in>
2025-08-18 20:37:56 +0530

    Initialize Django hospital project with core app
    Set up a new Django project named 'hospital' with a 'core' app. Added models for Doctor and Patient, admin registration, initial migrations, and basic project configuration including REST framework setup. Includes initial project structure, settings, and database.

---
+++ hospital/core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Doctor(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    specialization=models.CharField(max_length=100)
+    phone=models.CharField(max_length=15)
+
+    def __str__(self):
+        return f"Dr.{self.user.username} ({self.specialization})"
+    
+
+class Patient(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    age=models.IntegerField()
+    address=models.TextField()
+
+    def __str__(self):
+        return self.user.username
